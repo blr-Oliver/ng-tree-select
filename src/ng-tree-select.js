bd5ceeae13610109ebb93422f6f651f5476a4780
blr-Oliver <vasily.liaskovsky@gmail.com>
2017-11-12 02:51:22 +0300

    core source

---
+++ src/ng-tree-select.js
@@ -0,0 +1,73 @@
+angular.module('ngTreeSelect', ['RecursionHelper', 'ngCssInjector', 'ngSlideAnimations']).directive('treeSelect',
+    ['RecursionHelper', function(RecursionHelper){
+      return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'ng-tree-select.html',
+        cssUrl: 'ng-tree-select.css',
+        scope: {
+          items: '=',
+          depth: '=?'
+        },
+        compile: function(element){
+          return RecursionHelper.compile(element, function($scope){
+            $scope.depth = $scope.depth || 0;
+          });
+        }
+      }
+    }]).directive('booleanRadio', ['$document', '$parse', function($document, $parse){
+  var filter = Array.prototype.filter;
+  return {
+    restrict: 'A',
+    link: function($scope, element, attrs){
+      var target = attrs.booleanRadio;
+      element.prop('checked', $parse(target)($scope));
+      element.bind('click change input', function(){
+        //TODO add direct DOM property check
+        $scope.$apply(target + ' = true');
+        //recompute selector each time because name attribute itself may change
+        var selector = 'input[name="' + element.attr('name') + '"]';
+        angular.element(filter.call($document[0].querySelectorAll(selector), function(e){
+          return e !== element[0];
+        })).triggerHandler('deselect');
+      });
+      element.bind('deselect', function(){
+        $scope.$apply(target + ' = false');
+      })
+    }
+  }
+}]).directive('booleanSelect', [function(){
+  return {
+    restrict: 'A',
+    link: function($scope, element, attrs, controller){
+      element.on('change input blur', function(){
+        for (var i = this.options.length; i--;){
+          var option = this.options[i];
+          var optionController = angular.element(option).controller('booleanSelectOption');
+          optionController && optionController.update(option.selected);
+        }
+      });
+    }
+  }
+}]).directive('booleanSelectOption', [function(){
+  return {
+    restrict: 'A',
+    require: 'booleanSelectOption',
+    controller: ['$scope', function($scope){
+      this.update = function(newValue){
+        $scope.$applyAsync(this.targetProperty + ' = ' + !!newValue);
+      }
+    }],
+    link: function($scope, element, attrs, controller){
+      controller.targetProperty = attrs.booleanSelectOption;
+    }
+  }
+}]).animation('.tree-select', ['slideAnimations', function(slideAnimations){
+  return {
+    enter: slideAnimations.down,
+    leave: slideAnimations.up,
+  }
+}]).run(['$templateCache', function($templateCache){
+  $templateCache.put('ng-tree-select.html', 'TEMPLATE');
+  $templateCache.put('ng-tree-select.css', 'STYLE');
+}]);
